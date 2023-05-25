617f1e925589f9c8b598a3a226993bbbdaf3c9bf
Syssos <paral.cody98@gmail.com>
2023-05-25 16:12:02 -0700

    updated function comments

--- Frontend/v1/run.py
+++ Frontend/v1/run.py
@@ -11,32 +11,29 @@ app = Flask(__name__,
 cors = CORS(app, resources={"/*": {"origins": "127.0.0.1"}})
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def return_homePage():
-        ''' Returns api status
+        ''' Return the Homepage
         '''
-
         return render_template('index.html')
 
-@app.route('/browse')
-def return_category():
-        ''' Returns api status
+@app.route('/browse', methods=['GET'])
+def return_browse():
+        ''' Returns the Browse Page
         '''
-
         return render_template('browse.html')
 
-@app.route('/view')
-def return_item():
-        ''' Returns api status
+@app.route('/view', methods=['GET'])
+def return_view():
+        ''' Returns the View Page
         '''
-
         return render_template('view.html')
 
 @app.errorhandler(404)
 def errorhandler(error):
-    """ Returns status 404 Not found
-    """
-    return jsonify({'error': 'Not found'}), 404
+        ''' Returns status 404 Not found
+        '''
+        return jsonify({'error': 'Not found'}), 404
 
 if __name__ == "__main__":
     hosts = os.getenv("API_HOST", default="127.0.0.1")
